6473f74d3d2a4cd25186e5504df334d8284e2e59
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-03-03 21:41:50 -0500

    More Functions

--- backend/pool.sol
+++ backend/pool.sol
@@ -22,9 +22,11 @@ contract RidePool{
     struct rider {
         uint idr;
         userType uType;
-        address payable riderAddress;
+        address riderAddress;
         coordinates source;
         coordinates destination;
+        bool pickedUp;
+        bool droppedOff;
         bool allowPooling;
     }
 
@@ -33,6 +35,7 @@ contract RidePool{
         userType uType;
         address payable driverAddress;
         coordinates currentLocation;
+        bool occupied;
     }
     
 
@@ -47,11 +50,13 @@ contract RidePool{
         riderCounter++;
         riderAddressMapping[msg.sender].idr = riderCounter;
         riderAddressMapping[msg.sender].uType = userType.RIDER;
-        riderAddressMapping[msg.sender].riderAddress = payable(msg.sender);
+        riderAddressMapping[msg.sender].riderAddress = msg.sender;
         riderAddressMapping[msg.sender].source.latitude = slat;
         riderAddressMapping[msg.sender].source.longitude = slong;
         riderAddressMapping[msg.sender].destination.latitude = dlat;
         riderAddressMapping[msg.sender].destination.latitude = dlong;
+        riderAddressMapping[msg.sender].pickedUp = false;
+        riderAddressMapping[msg.sender].droppedOff = false;
         riderAddressMapping[msg.sender].allowPooling = false;
     }
 
@@ -63,11 +68,49 @@ contract RidePool{
         driverAddressMapping[msg.sender].driverAddress = payable(msg.sender);
         driverAddressMapping[msg.sender].currentLocation.latitude = clat;
         driverAddressMapping[msg.sender].currentLocation.longitude = clong;
+        driverAddressMapping[msg.sender].occupied = false;
+
+        driversList.push(msg.sender);
     }
 
     function selectRide() public {
-
+        address closestDriver = findClosest(msg.sender);
+        driverAddressMapping[closestDriver].occupied = true;
     }
 
+    function findClosest(address riderAddressParam) view private returns (address) {
+        uint256 dListLength = driversList.length;
+
+        int256 rSLat = riderAddressMapping[riderAddressParam].source.latitude;
+        int256 rSLong = riderAddressMapping[riderAddressParam].source.longitude;
+
+        int256 compare = 99999999999999;
+        address closestDriver = 0x0000000000000000000000000000000000000000;
+
+        for (uint256 i = 0; i < dListLength; i++){
+            address currDriverAddress = driversList[i];
+            
+            if (driverAddressMapping[currDriverAddress].occupied == false){
+
+                int256 dCLat = driverAddressMapping[currDriverAddress].currentLocation.latitude;
+                int256 dCLong =  driverAddressMapping[currDriverAddress].currentLocation.longitude;
+                int256 distRD = manhattanDistance(rSLat, rSLong, dCLat, dCLong);
+
+                if (compare > distRD){
+                    compare = distRD;
+                    closestDriver = currDriverAddress;
+                }
+            }
+        }
+        return closestDriver;
+    }
+
+    function manhattanDistance(int256 slat, int256 slong, int256 dlat, int256 dlong) pure private returns (int256){
+        int256 latF = slat - dlat;
+        int256 longF = slong - dlong;
+        if (latF < 0) {latF = -latF;}
+        if (longF < 0) {longF = -longF;}
+        return latF+longF;
+    }
 
 }
