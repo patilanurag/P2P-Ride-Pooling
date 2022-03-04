a6aeeb12b336dab9a4a312e0c952b6abcd46b8fe
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-03-03 20:45:03 -0500

    Modified Structure

--- backend/pool.sol
+++ backend/pool.sol
@@ -4,6 +4,10 @@ pragma solidity >=0.5.0;
 
 contract RidePool{
 
+    uint riderCounter = 0;
+
+    uint driverCounter = 0;
+
     constructor () {
 
     }
@@ -16,7 +20,8 @@ contract RidePool{
     }
 
     struct rider {
-        int idr;
+        uint idr;
+        userType uType;
         address payable riderAddress;
         coordinates source;
         coordinates destination;
@@ -24,7 +29,8 @@ contract RidePool{
     }
 
     struct driver {
-        int idd;
+        uint idd;
+        userType uType;
         address payable driverAddress;
         coordinates currentLocation;
     }
@@ -34,15 +40,34 @@ contract RidePool{
     mapping(address=>driver) driverAddressMapping;
     // mapping(address=>driver) driverAddressMapping;
     address [] public driversList;
-    address [] public ridersList;
+    // address [] public ridersList;
 
-    function requestRide() public {
-        // select source and destination
+    function setRider(int256 slat, int256 slong, int256 dlat, int256 dlong) public {
+        // set information related to the rider
+        riderCounter++;
+        riderAddressMapping[msg.sender].idr = riderCounter;
+        riderAddressMapping[msg.sender].uType = userType.RIDER;
+        riderAddressMapping[msg.sender].riderAddress = payable(msg.sender);
+        riderAddressMapping[msg.sender].source.latitude = slat;
+        riderAddressMapping[msg.sender].source.longitude = slong;
+        riderAddressMapping[msg.sender].destination.latitude = dlat;
+        riderAddressMapping[msg.sender].destination.latitude = dlong;
+        riderAddressMapping[msg.sender].allowPooling = false;
+    }
 
+    function setDriver(int256 clat, int256 clong) public {
+        // set information related to the driver
+        driverCounter++;
+        driverAddressMapping[msg.sender].idd = driverCounter;
+        driverAddressMapping[msg.sender].uType = userType.DRIVER;
+        driverAddressMapping[msg.sender].driverAddress = payable(msg.sender);
+        driverAddressMapping[msg.sender].currentLocation.latitude = clat;
+        driverAddressMapping[msg.sender].currentLocation.longitude = clong;
     }
 
     function selectRide() public {
 
     }
+
 
 }
